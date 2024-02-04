b9aa2a594be030263cbabc29a90babb1f7194e39
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-04 17:55:05 -0500

    Update ImageReading.py

--- ImageReading.py
+++ ImageReading.py
@@ -1,29 +1,14 @@
 #Imports necessary packages
-import cv2 as cv
-
-#Imports function for reading images
+import ImageReading
 import ImageProcessing
 import ImageDisplaying
 
-def readImage(img):
-    #Variable needed for displaying the video
-    videoIsPlaying = True
+#Loads the image
+file = "rollingCan.mov"
+circle = ImageReading.readImage(file)
 
-    #Starts the video capture
-    video = cv.VideoCapture(img)
+#Applies image detection
+ImageProcessing.processImage(circle)
 
-    #Prevents program from crashing error
-    try:
-        count = 0
-        #While the video is playing, read the frame, process it & display it
-        while videoIsPlaying == True:
-            videoIsPlaying, frame = video.read()
-            ImageProcessing.processImage(frame)
-            if count % 5 == 0:
-                ImageDisplaying.displayImage(frame)
-            count += 1
-        cv.destroyAllWindows()
-
-    #Prints message when video is done
-    except: print("The video is done.")
-    finally: exit()
+#Displays image with hough lines
+ImageDisplaying.displayImage(circle)
