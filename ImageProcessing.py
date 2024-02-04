f086747c3fe6804e03a5df6afe24d4bc69b169af
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-04 16:37:22 -0500

    Update ImageProcessing.py

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -3,15 +3,14 @@ import cv2 as cv
 import numpy as np
 
 def processImage(image):
-    # Applies gaussian blur and canny edge detection on image
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    gray_scale = cv.GaussianBlur(gray, (15, 15), 0)
-    canny_image = cv.Canny(gray_scale, 100, 100)
+    # Applies median blur and canny edge detection on image
+    median_blur = cv.medianBlur(image, 5)
+    canny_image = cv.Canny(median_blur, 100, 100)
 
     # Creates hough lines around image
-    circle = cv.HoughCircles(canny_image, cv.HOUGH_GRADIENT, 8, 5000,
-                             param1 = 50, param2 = 45,
-                             minRadius = 300, maxRadius = 300)
+    circle = cv.HoughCircles(canny_image, cv.HOUGH_GRADIENT, 12, 2000,
+                             param1 = 45, param2 = 45,
+                             minRadius = 265, maxRadius = 270)
 
     #If the circle is detected, display them
     if circle is not None:
@@ -19,6 +18,6 @@ def processImage(image):
         circle_arr = np.uint16(np.round(circle))
         for point in circle_arr[0, :]:
             # Creates the circle around the image
-            cv.circle(image, (point[0] + 5, point[1] - 14), point[2] + 15, (0, 255, 0), 5)
+            cv.circle(image, (point[0], point[1]), point[2], (0, 255, 0), 10)
             # Finds the center point of the circle
-            cv.circle(image, (point[0] + 5, point[1] - 14), 2, (0, 0, 255), 5)
+            cv.circle(image, (point[0], point[1]), 2, (0, 0, 255), 5)
