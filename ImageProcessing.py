929148429250cb828032498af9424807128d627c
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-04 13:02:50 -0500

    Update ImageProcessing.py

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,26 +1,36 @@
-#Imports necessary packages
+# Imports necessary packages
 import cv2 as cv
+import numpy as np
 
-#Imports function for reading images
-import ImageProcessing
-import ImageDisplaying
+#Creates a mask around the frame
+def crop_image(image, vertices):
+    #Defines the mask using numpy
+    mask = np.zeros_like(image)
+    #Fills the mask
+    mask_color = 255
+    cv.fillPoly(mask, vertices, mask_color)
+    masked_image = cv.bitwise_and(image, mask)
+    #Returns the mask when called
+    return masked_image
 
-def readImage(img):
-    #Variable needed for displaying the video
-    videoIsPlaying = True
+def processImage(masked_image):
+    # Applies gaussian blur and canny edge detection on image
+    gray = cv.cvtColor(masked_image, cv.COLOR_BGR2GRAY)
+    gray_scale = cv.GaussianBlur(gray, (15, 15), 0)
+    canny_image = cv.Canny(gray_scale, 100, 120)
 
-    #Starts the video capture
-    video = cv.VideoCapture(img)
+    # Creates hough lines around image
+    circle = cv.HoughCircles(canny_image, cv.HOUGH_GRADIENT, 145, 1000,
+                             param1=100, param2=200,
+                             minRadius= 150, maxRadius=175)
 
-    #Prevents program from crashing error
-    try:
-        #While the video is playing, read the frame, process it & display it
-        while videoIsPlaying == True:
-                videoIsPlaying, image = video.read()
-                ImageProcessing.processImage(image)
-                ImageDisplaying.displayImage(image)
-        return image
-    #Print
-    except: print("The video is done.")
-    finally: exit()
+    #If the circle is detected, display them
+    if circle is not None:
+        #Creates array of points around the circle using the hough lines
+        circle_arr = np.uint16(np.round(circle))
 
+        for point in circle_arr[0, :]:
+            # Creates the circle around the image
+            cv.circle(masked_image, (point[0], point[1]), point[2], (0, 255, 0), 5)
+            # Finds the center point of the circle
+            cv.circle(masked_image, (point[0], point[1]), 2, (0, 0, 255), 5)
