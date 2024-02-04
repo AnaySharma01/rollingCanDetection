7a311f1e34a59e48f15e9a172901ae51dd061d14
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-04 13:41:10 -0500

    Update ImageDisplaying.py

--- ImageDisplaying.py
+++ ImageDisplaying.py
@@ -2,9 +2,6 @@
 import cv2 as cv
 
 def displayImage(masked_image):
-    #Creates a numpy array for the processed image
-
     #Returns the processed image
     cv.imshow("circleWithDetection", masked_image)
     cv.waitKey(1)
-    cv.destroyAllWindows()
