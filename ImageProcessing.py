3ef793c6895552f54f6bec9270d863d3ffa69c3d
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-02-04 13:41:38 -0500

    Update ImageProcessing.py

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -2,17 +2,6 @@
 import cv2 as cv
 import numpy as np
 
-#Creates a mask around the frame
-def crop_image(image, vertices):
-    #Defines the mask using numpy
-    mask = np.zeros_like(image)
-    #Fills the mask
-    mask_color = 255
-    cv.fillPoly(mask, vertices, mask_color)
-    masked_image = cv.bitwise_and(image, mask)
-    #Returns the mask when called
-    return masked_image
-
 def processImage(masked_image):
     # Applies gaussian blur and canny edge detection on image
     gray = cv.cvtColor(masked_image, cv.COLOR_BGR2GRAY)
@@ -21,8 +10,8 @@ def processImage(masked_image):
 
     # Creates hough lines around image
     circle = cv.HoughCircles(canny_image, cv.HOUGH_GRADIENT, 145, 1000,
-                             param1=100, param2=200,
-                             minRadius= 150, maxRadius=175)
+                             param1 = 100, param2 = 200,
+                             minRadius = 300, maxRadius = 400)
 
     #If the circle is detected, display them
     if circle is not None:
